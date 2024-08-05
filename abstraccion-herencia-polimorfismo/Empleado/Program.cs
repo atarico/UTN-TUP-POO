f5196bc414ce9e2cc985f00547f69c11dbcc33b2
atarico9 <atarico9@gmail.com>
2024-08-05 19:16:50 -0300

    Práctica Abstracción, Herencia y Polimorfismo Vehiculo, FormaGeometrica, DispositivoElectronico, Empleado

---
+++ abstraccion-herencia-polimorfismo/Empleado/Program.cs
@@ -0,0 +1,43 @@
+﻿abstract class Empleado
+{
+    public string Nombre { get; set; }
+    public double Salario { get; set; }
+
+    public Empleado(string nombre, double salario)
+    {
+        Nombre = nombre;
+        Salario = salario;
+    }
+
+    public abstract double CalcularSalario();
+}
+
+class EmpleadoTiempoCompleto : Empleado
+{
+    public EmpleadoTiempoCompleto(string nombre, double salario) : base(nombre, salario) { }
+
+    public override double CalcularSalario()
+    {
+      return Salario;
+    }
+}
+
+class EmpleadoTiempoParcial : Empleado
+{
+    public EmpleadoTiempoParcial(string nombre, double salario) : base(nombre, salario) { }
+    public override double CalcularSalario() {
+      return Salario / 2;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Empleado unEmpleadoTiempoCompleto = new EmpleadoTiempoCompleto("Pepito", 1000);
+        Empleado unEmpleadoTiempoParcial = new EmpleadoTiempoParcial("Jorgito", 1000);
+
+        Console.WriteLine($"El salario de {unEmpleadoTiempoCompleto.Nombre} es: {unEmpleadoTiempoCompleto.CalcularSalario()}");
+        Console.WriteLine($"El salario de {unEmpleadoTiempoParcial.Nombre} es: {unEmpleadoTiempoParcial.CalcularSalario()}");
+    }
+}
---
+++ abstraccion-herencia-polimorfismo/Vehiculo/Program.cs
@@ -0,0 +1,45 @@
+﻿abstract class Vehiculo
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+
+    public Vehiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    public abstract void Conducir();
+}
+
+
+class Auto : Vehiculo
+{
+    public Auto(string marca, string modelo) : base(marca, modelo) { }
+    public override void Conducir()
+    {
+        Console.WriteLine($"Usted está conduciendo un auto {Marca} {Modelo}");
+    }
+}
+
+class Moto : Vehiculo
+{
+    public Moto(string marca, string modelo) : base(marca, modelo) { }
+    public override void Conducir()
+    {
+        Console.WriteLine($"Usted está conduciendo una moto {Marca} {Modelo}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vehiculo elAutoDePepito = new Auto("Tesla", "Model S");
+        elAutoDePepito.Conducir();
+
+        Vehiculo laMotoDePepita = new Moto("Yamaha", "Trastornado");
+        laMotoDePepita.Conducir();
+    }
+}
+
