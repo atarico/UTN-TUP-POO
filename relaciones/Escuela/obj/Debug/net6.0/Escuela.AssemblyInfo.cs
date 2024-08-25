6f99f005f899957d26d412c7d7be6b24a9141a54
atarico9 <atarico9@gmail.com>
2024-08-24 23:05:55 -0300

    Escuelita

--- relaciones/Escuela/obj/Debug/net6.0/Escuela.AssemblyInfo.cs
+++ relaciones/Escuela/obj/Debug/net6.0/Escuela.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Escuela")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e59a0d7de9d53f6263ece8960ef1c3e9a967b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Escuela")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Escuela")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
