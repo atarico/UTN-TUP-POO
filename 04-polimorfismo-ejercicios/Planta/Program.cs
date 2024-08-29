8451659b0e00e413360a839b9cfc01beab4852f5
atarico9 <atarico9@gmail.com>
2024-08-28 21:08:25 -0300

    Planta, Publicacion, Transporte

---
+++ 04-polimorfismo-ejercicios/Planta/Program.cs
@@ -0,0 +1,53 @@
+﻿class Planta
+{
+    public string Nombre { get; set; }
+    public Planta(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    public virtual void Florecer()
+    {
+        Console.WriteLine("La planta florece");
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Planta: {Nombre}");
+        Florecer();
+    }
+}
+
+class Arbol : Planta
+{
+    public Arbol(string nombre) : base(nombre) { }
+
+    public override void Florecer()
+    {
+        Console.WriteLine("El árbol florece en verano");
+    }
+}
+
+class Flor : Planta
+{
+    public Flor(string nombre) : base(nombre) { }
+
+    public override void Florecer()
+    {
+        Console.WriteLine("La flor florece en primavera");
+    }
+}
+
+class Programa
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el nombre del árbol: ");
+        Planta unArbol = new Arbol(Console.ReadLine());
+        unArbol.MostrarInformacion();
+
+        Console.Write("Ingrese el nombre de la planta: ");
+        Planta unaFlor = new Flor(Console.ReadLine());
+        unaFlor.MostrarInformacion();
+    }
+}
---
+++ 04-polimorfismo-ejercicios/Transporte/Program.cs
@@ -0,0 +1,66 @@
+﻿class Transporte
+{
+    public string Nombre { get; set; }
+
+    public Transporte(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    public virtual void Desplazarse()
+    {
+        Console.WriteLine("El vehículo se desplaza");
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Vehículo: {Nombre}");
+        Desplazarse();
+    }
+}
+
+class Auto : Transporte
+{
+    public Auto(string nombre) : base(nombre) { }
+
+    public override void Desplazarse()
+    {
+        Console.WriteLine("El auto se desplaza por la autopista");
+    }
+}
+
+class Bicicleta : Transporte
+{
+    public Bicicleta(string nombre) : base(nombre) { }
+    public override void Desplazarse()
+    {
+        Console.WriteLine($"La bici se desplaza por la bicisenda");
+    }
+}
+
+class Avion : Transporte
+{
+    public Avion(string nombre) : base(nombre) { }
+    public override void Desplazarse()
+    {
+        Console.WriteLine("El avión se desplaza por el aire");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el nombre del auto: ");
+        Transporte unAuto = new Auto(Console.ReadLine() ?? ""); //-> Para quitar el warning de null, se puede usar el operador de coalescencia nula
+        unAuto.MostrarInformacion();
+
+        Console.Write("Ingrese el nombre de la bicicleta: ");
+        Transporte unaBicicleta = new Bicicleta(Console.ReadLine());
+        unaBicicleta.MostrarInformacion();
+
+        Console.Write("Ingrese el nombre del avión: ");
+        Transporte unAvion = new Avion(Console.ReadLine());
+        unAvion.MostrarInformacion();
+    }
+}
