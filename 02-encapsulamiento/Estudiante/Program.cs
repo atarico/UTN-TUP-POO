3b8930516bd948923940f648587ddbcc488c093e
atarico9 <atarico9@gmail.com>
2024-08-28 21:04:39 -0300

    Cta Bancaria, estudiante, persona, producto

---
+++ 02-encapsulamiento/Estudiante/Program.cs
@@ -0,0 +1,57 @@
+﻿class Estudiante
+{
+    private string _nombre = null!;
+    private int _legajo;
+    private double _promedio;
+
+    public string Nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+
+    public int Legajo
+    {
+        get { return _legajo; }
+        set { _legajo = value; }
+    }
+
+    public double Promedio
+    {
+        get { return _promedio; }
+        set
+        {
+            if(value >= 0 && value <= 10)
+            {
+                _promedio = value;
+            }
+            else
+            {
+                Console.WriteLine("El promedio debe ser mayor a 0 y menor a 10");
+            }
+        }
+    }
+
+    public Estudiante(string nombre, int legajo, double promedio)
+    {
+        _nombre = nombre;
+        _legajo = legajo;
+        Promedio = promedio;
+    }
+
+    public void ActualizarPromedio(double nuevoPromedio)
+    {
+        Promedio = nuevoPromedio;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Estudiante unEstudiante = new Estudiante("Pepito", 1234, 8.5);
+        Console.WriteLine($"Nombre: {unEstudiante.Nombre} \n Legajo: {unEstudiante.Legajo} \n Promedio: {unEstudiante.Promedio}");
+        unEstudiante.ActualizarPromedio(9.5);
+        Console.WriteLine($"Nombre: {unEstudiante.Nombre} \n Legajo: {unEstudiante.Legajo} \n Promedio Actualizado: {unEstudiante.Promedio}");
+    }
+}
---
+++ 02-encapsulamiento/Producto/Program.cs
@@ -0,0 +1,74 @@
+﻿class Producto
+{
+    private string _nombre;
+    private decimal _precio;
+    private int _cantidadEnStock;
+
+    public Producto(string nombre, decimal precio, int cantidadEnStock)
+    {
+        _nombre = nombre;
+        _precio = precio;
+        _cantidadEnStock = cantidadEnStock;
+    }
+
+    public string Nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+
+    public decimal Precio
+    {
+        get { return _precio; }
+        set { _precio = value; }
+    }
+
+    public int CantidadEnStock
+    {
+        get { return _cantidadEnStock; }
+        set { _cantidadEnStock = value; }
+    }
+
+    public void AumentarStock(int cantidad)
+    {
+        if (cantidad <= 0)
+        {
+            Console.WriteLine("La cantidad a agregar debe ser mayor a 0");
+        }
+        else
+        {
+            _cantidadEnStock += cantidad;
+            Console.WriteLine($"Stock agregado exitosamente \n Cantidad agregada: {cantidad} \n Stock actual: {_cantidadEnStock}");
+        }
+    }
+
+    public void DisminuirStock(int cantidad)
+    {
+        if (cantidad <= 0)
+        {
+            Console.WriteLine("La cantidad a retirar debe ser mayor a 0");
+        }
+        else if (cantidad > _cantidadEnStock)
+        {
+            Console.WriteLine("No se puede retirar más de lo que hay en stock");
+        }
+        else
+        {
+            _cantidadEnStock -= cantidad;
+            Console.WriteLine($"Retiro exitoso \n Cantidad retirada: {cantidad} \n Stock actual: {_cantidadEnStock}");
+        }
+    }
+}
+
+class Program {
+    static void Main()
+    {
+        Producto unProducto = new Producto("Laptop", 1200, 50);
+        Console.WriteLine($"Nombre: {unProducto.Nombre} \n Precio: {unProducto.Precio} \n Stock: {unProducto.CantidadEnStock}");
+
+        unProducto.AumentarStock(10);
+        //unProducto.AumentarStock(-70);
+
+        unProducto.DisminuirStock(20);
+    }
+}
